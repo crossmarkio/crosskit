5d18cfeea2f646bc7009f709d5eb40bbf9130917
interc0der <intercoder@whirled.io>
2023-06-11 20:16:25 -0700

    Update: remove template forlder preventing build

--- src/components/general/template/index.tsx
+++
@@ -1,7 +0,0 @@
-import Dash from './dash';
-import Admin from './admin';
-import App from './app';
-import Landing from './landing';
-import Modal from './modal';
-
-export { Dash, Admin, App, Landing, Modal };
